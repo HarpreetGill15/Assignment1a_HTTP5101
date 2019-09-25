55f80e4af506638a0e4ec46f6e4b095ba0f634d2
Harpreet Gill <harpreet_gill_@outlook.com>
2019-09-24 22:03:07 -0400

    Validation Assignment

--- index_WebForm_Server_Controls.aspx
+++ index_WebForm_Server_Controls.aspx
@@ -31,14 +31,16 @@
                 <asp:TextBox runat="server" ID="client_phone_number"></asp:TextBox>
                 <asp:RegularExpressionValidator runat="server" EnableClientScript="true"  ErrorMessage="Please enter a valid phone number!" ForeColor="Red" ControlToValidate="client_phone_number" ValidationExpression="((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}"></asp:RegularExpressionValidator>
             </div>
+            
             <div>
                 <asp:Label runat="server">Date Of Birth:</asp:Label>
                 <asp:TextBox runat="server" ID="client_date_of_birth_month" style="width:10%"></asp:TextBox>
-                <asp:RangeValidator runat="server" EnableClientScript="true"  ErrorMessage="Please enter a valid month" ForeColor="Red" ControlToValidate="client_date_of_birth_month" MaximumValue="12" MinimumValue="1"></asp:RangeValidator>
+                <!-- Adding type=integer due to some unforseen problems with the month not being valid from 2-9 for some odd reason. This was taken from a friend of mine. -->
+                <asp:RangeValidator runat="server" ControlToValidate="client_date_of_birth_month" Text="Please enter a valid month" MaximumValue="12" MinimumValue="1" EnableClientScript="true" Type="Integer"></asp:RangeValidator>
                 <asp:TextBox runat="server" ID="client_date_of_birth_day" style="width:10%"></asp:TextBox>
                 <asp:RangeValidator runat="server" EnableClientScript="true"  ErrorMessage="Please enter a valid day" ForeColor="Red" ControlToValidate="client_date_of_birth_day" MaximumValue="31" MinimumValue="1"></asp:RangeValidator>
                 <asp:TextBox runat="server" ID="client_date_of_birth_year" style="width:10%"></asp:TextBox>
-                <asp:RangeValidator runat="server" EnableClientScript="true"  ErrorMessage="Please enter a valid year (Must be 16 years of age)" ForeColor="Red" ControlToValidate="client_date_of_birth_day" MaximumValue="2013" MinimumValue="1869"></asp:RangeValidator>
+                <asp:RangeValidator runat="server" EnableClientScript="true"  ErrorMessage="Please enter a valid year (Must be 16 years of age)" ForeColor="Red" ControlToValidate="client_date_of_birth_year" MaximumValue="2013" MinimumValue="1869"></asp:RangeValidator>
             </div>
             <div>
                 <asp:Label runat="server">Experience:</asp:Label>
@@ -51,9 +53,8 @@
             <div>
                 <asp:Label runat="server">Jump Type:</asp:Label>
                 <asp:RadioButtonList runat="server" ID="client_jump_type">
-                    <asp:ListItem Text="10,500 feet (40 seconds of freefall)" Value="standard"></asp:ListItem>
-                    <asp:ListItem Text="Some Experience" Value="Some Experience"></asp:ListItem>
-                    <asp:ListItem Text="Veteran at Skydiving" Value="Veteran"></asp:ListItem>
+                    <asp:ListItem Text="10,500 feet (40 seconds of freefall)" Value="275"></asp:ListItem>
+                    <asp:ListItem Text="14,000 feet (60 seconds of freefall)" Value="299"></asp:ListItem>
                 </asp:RadioButtonList>
             </div>
            <div>
--- index_WebForm_Server_Controls.aspx.cs
+++ index_WebForm_Server_Controls.aspx.cs
@@ -16,34 +16,60 @@ namespace Assingment1a_HTTP5101
                 Page.Validate();
                 if (Page.IsValid)
                 {
-                    string client_Fname = client_first_name.ToString();
-                    string client_Lname = client_last_name.ToString();
-                    string client_Email = client_email.ToString();
-                    string client_Phone = client_phone_number.ToString();
-                    string client_DOB = client_date_of_birth_day.ToString() + client_date_of_birth_month.ToString() + client_date_of_birth_year.ToString();
-                    string client_Experience = client_experience.SelectedValue;
-                    int client_Jumpers = Convert.ToInt32(client_jumpers.SelectedValue);
-                    string client_Video = client_record_me.SelectedValue;
-                    string client_Mailing = client_mailing.SelectedValue;
+                    string client_Fname = client_first_name.Text.ToString();
+                    string client_Lname = client_last_name.Text.ToString();
+                    string client_Email = client_email.Text.ToString();
+                    string client_Phone = client_phone_number.Text.ToString();
+                    string client_DOB = client_date_of_birth_month.Text.ToString() + "/" + client_date_of_birth_day.Text.ToString() + "/" + client_date_of_birth_year.Text.ToString();
+                    int client_Age = 2019 - Convert.ToInt32(client_date_of_birth_year.Text.ToString());
+                    string client_Experience = client_experience.SelectedValue.ToString();
+                    int client_Jump = Convert.ToInt32(client_jump_type.SelectedValue.ToString());
+                    int client_Jumpers = Convert.ToInt32(client_jumpers.SelectedValue.ToString());
+                    string client_Video = client_record_me.SelectedValue.ToString();
+                    string client_Mailing = client_mailing.SelectedValue.ToString();
+                    double price = 0.0;
 
-                    confirmbox.InnerHtml = "Thank you for booking your jump!";
+                    confirmbox.InnerHtml = "Thank you for booking your jump!<br>";
 
-                    confirmbox.InnerHtml += "First Name: " + client_Fname + "";
-                    confirmbox.InnerHtml += "Last Name: " + client_Lname + "";
-                    confirmbox.InnerHtml += "Email: " + client_Email + "";
-                    confirmbox.InnerHtml += "Phone Number: " + client_Phone + "";
-                    confirmbox.InnerHtml += "First Name: " + client_Fname + "";
-                    confirmbox.InnerHtml += "Date of Birth: " + client_DOB + "";
-                    confirmbox.InnerHtml += "Number of jumpers: " + client_Jumpers + "";
+                    confirmbox.InnerHtml += "First Name: " + client_Fname + "<br>";
+                    confirmbox.InnerHtml += "Last Name: " + client_Lname + "<br>";
+                    confirmbox.InnerHtml += "Email: " + client_Email + "<br>";
+                    confirmbox.InnerHtml += "Phone Number: " + client_Phone + "<br>"; 
+                    confirmbox.InnerHtml += "Date of Birth: " + client_DOB + "<br>";
+                    confirmbox.InnerHtml += "Number of jumpers: " + client_Jumpers + "<br>";
 
-                    confirmbox.InnerHtml += "Estimated price calculated is: ";
-                    switch (client_Jumpers)
+                    confirmbox.InnerHtml += "Estimated price calculated is: <br>";
+
+                    /*If the client is under the age of 18 there is an addtional fee */
+                    if (client_Age < 18)
                     {
-                        case 1:
-                            confirmbox.InnerHtml += ""
+                        price += 90.00;
+                        confirmbox.InnerHtml += "Minor Jumper price: $90.00<br>";
+
                     }
-
-
+                    /*If the client wants to record the jump add the fee of recording and editing the movie*/
+                    if (client_Video == "Yes_Record")
+                    {
+                        price += 100.00;
+                        confirmbox.InnerHtml += "Videography Fee: $100.00<br>";
+                    }
+                    /*If the client comes with a group more then or equal to 3 add a fee*/
+                    if (client_Jumpers >=3)
+                    {
+                        price += 50.00;
+                        confirmbox.InnerHtml += "Group jump (more then 2 jumpers): $50.00<br>";
+                    }
+                    if(client_Jump == 275)
+                    {
+                        price += 275.00;
+                        confirmbox.InnerHtml += "Standard jump (10,500 feet, 40 second free fall): $275.00<br>";
+                    }
+                    else if(client_Jump == 299)
+                    {
+                        price += 299.00;
+                        confirmbox.InnerHtml += "Plus Jump (14,000 feet, 60 second free fall): $299.00<br>";
+                    }
+                    confirmbox.InnerHtml += "Total price: $"+price;
                 }
             }
         }
